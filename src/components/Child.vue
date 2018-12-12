933d3a10d52e229faebd8d7f6df58c7e4ae8202a
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-12-13 10:45:50 +1300

    testing vue basic af

---
+++ src/components/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="child">
+    <p>{{ testData }}</p>
+    <button v-on:click='changeTestData'>change test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  props: {
+    testData: String,
+    changeTestData: Function
+  }
+}
+</script>
